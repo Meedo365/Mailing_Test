1e40769479dee9b79e2d6d80ca708d575fd3371b
Meedo365 <nuhuahmed365@gmail.com>
2024-04-28 19:43:43 +0100

    Create NotFound.js

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+function NotFound() {
+  return (
+    <>
+      <title>Not Found Page</title>
+      <meta name="description" content="Not Found Page to the Assessment" />
+      <div className="container mx-auto">
+        <div className="flex justify-center items-center h-screen">
+          <h1 class="text-4xl font-bold text-center text-black">
+            404 <br /> Not Found
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
