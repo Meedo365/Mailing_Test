18dba0a47e4e5dd5d8899661f0ffd848f27197e0
Meedo365 <nuhuahmed365@gmail.com>
2024-04-28 17:12:56 +0100

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Button, Card, Label, TextInput } from "flowbite-react";
+import { Store } from "../context/store";
+import { useNavigate } from "react-router-dom";
 function Login() {
+  let store = useContext(Store);
+  let [baseUrl] = store.url;
+  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
+  let [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  let login = () => {
+    let url = `${baseUrl}/login`;
+
+    if (!email || !password) {
+      setError("Please provide both email and password.");
+      return;
+    }
+
+    let data = {
+      password,
+      email,
+    };
+    fetch(url, {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status) {
+          localStorage.setItem("inbox", result.data);
+          navigate("/home");
+        } else {
+          setError(result.message);
+        }
+      })
+      .catch((error) => {
+        console.error("Error during login:", error);
+        setError("An error occurred during login. Please try again.");
+      });
+  };
+  
   return (
     <>
       <title>Login Page</title>
@@ -9,7 +52,8 @@ function Login() {
         <div className="flex justify-center items-center h-screen">
           <Card className="w-3/4 md:w-1/2 lg:w-1/4">
             <h3 className="text-center font-extrabold text-2xl">Login</h3>
-            <form className="flex max-w-md flex-col gap-4">
+            <p className="text-red-500 text-sm text-center">{error}</p>
+            <div className="flex max-w-md flex-col gap-4">
               <div>
                 <div className="mb-2 block">
                   <Label htmlFor="email1" value="Your email" />
@@ -19,6 +63,9 @@ function Login() {
                   type="email"
                   placeholder="Enter Email"
                   required
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div>
@@ -30,10 +77,13 @@ function Login() {
                   type="password"
                   required
                   placeholder="Enter Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <Button type="submit">Submit</Button>
-            </form>
+              <Button onClick={() => login()}>Submit</Button>
+            </div>
           </Card>
         </div>
       </div>
