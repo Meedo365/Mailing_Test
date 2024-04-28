bd53f8564fff7d8ac3a5b1e81fea5ba432a5d2e2
Meedo365 <nuhuahmed365@gmail.com>
2024-04-28 15:41:39 +0100

    Update Nav.js

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function NavbarWithCTAButton() {
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand as={Link} href="/home">
+      <Navbar.Brand as={Link} to={"/home"}>
         <img src={img} className="mr-3 h-6 sm:h-9 rounded-full" alt="Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Assessment
@@ -16,7 +16,7 @@ export default function NavbarWithCTAButton() {
         <Navbar.Link href="/home" active>
           Home
         </Navbar.Link>
-        <Navbar.Link className="cursor-pointer">Sign Out</Navbar.Link>
+        <Navbar.Link className="cursor-pointer" href="/">Sign Out</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
