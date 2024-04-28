61ea85c6e5128ccf8850142df3dec3e4893a0740
Meedo365 <nuhuahmed365@gmail.com>
2024-04-28 15:21:23 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/message" element={<Message />} />
       </Routes>
     </Router>
