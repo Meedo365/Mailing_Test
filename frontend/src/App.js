d95e52c7b4f3eb8891ddf5b04c06c9b4e2665d71
Meedo365 <nuhuahmed365@gmail.com>
2024-04-28 17:12:41 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,41 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
 import "./assets/css/style.css";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Message from "./pages/Message";
+import StoreContext from "./context/store";
 
 function App() {
+  const UploadRouteGuard = ({ element }) => {
+    if (
+      !localStorage.getItem("inbox") ||
+      localStorage.getItem("inbox") === "undefined"
+    ) {
+      alert("Access denied. Please login.");
+      return <Navigate to="/" />;
+    }
+    return element;
+  };
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/message" element={<Message />} />
-      </Routes>
-    </Router>
+    <StoreContext>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={<UploadRouteGuard element={<Home />} />}
+          />
+          <Route
+            path="/message"
+            element={<UploadRouteGuard element={<Message />} />}
+          />
+        </Routes>
+      </Router>
+    </StoreContext>
   );
 }
 
