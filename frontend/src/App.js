052f577d1b3e0950126ded6c729db9aa8af8c54b
Meedo365 <nuhuahmed365@gmail.com>
2024-04-28 19:38:17 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,28 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Message from "./pages/Message";
 import StoreContext from "./context/store";
+import { useState } from "react";
+import { Toast } from "flowbite-react";
+import { HiX } from "react-icons/hi";
 
 function App() {
+  let [error, setError] = useState("");
   const UploadRouteGuard = ({ element }) => {
     if (
       !localStorage.getItem("inbox") ||
       localStorage.getItem("inbox") === "undefined"
     ) {
+      setError(
+        <Toast>
+          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200">
+            <HiX className="h-5 w-5" />
+          </div>
+          <div className="ml-3 text-sm font-normal">
+            Access denied. Please login.
+          </div>
+          <Toast.Toggle />
+        </Toast>
+      );
       return <Navigate to="/" />;
     }
     return element;
@@ -24,7 +39,7 @@ function App() {
     <StoreContext>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login error={error} />} />
           <Route
             path="/home"
             element={<UploadRouteGuard element={<Home />} />}
